387fc47930337bdcab7662c2c2b4051931c2f701
Ivan Orellana Jr <iorellana21@gmail.com>
2021-01-23 11:52:19 -0500

    more updates to html, css, js, readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# work-planner
-Work Planner
+# Work Planner
+## Description
+This app is intended to be used to schedule out a users workday by entering tasks for each hour and saving to their local storage.
+
+If the current hour is greater than the timeslot, those will appear in grey.
+If the current hour is less than the timeslot, those will appear in green.
+If the current hour is equal to a timeslot, that will appear in red.
+
+## What Was Done
+* Modify HTML page and create time blocks
+* Build JS page for color-coding rows and saving/retrieving data
+* Use localStorage
+
+---
+## Process
+User can go to page and enter in tasks for working hours.
+* When a user clicks the Save button:
+    1. Time and task entered are saved into local storage
+    2. First question appears, waits for user selection
+        * if user chooses correct, score will increase by 1
+        * if user chooses incorrect, score does not increase, timer is deducted by 5 seconds
+
+## Main Page
+![alt text](https://raw.githubusercontent.com/iorellana21/code-quiz/main/assets/code-quiz-main-page.png "code-quiz-main-page")
+
+## Questions Page
+![alt text](https://raw.githubusercontent.com/iorellana21/code-quiz/main/assets/Question.png "code-quiz-question-page")
+
+## User initials Page
+![alt text](https://raw.githubusercontent.com/iorellana21/code-quiz/main/assets/userInitials.png "code-quiz-initials-page")
+## Score Page
+![alt text](https://raw.githubusercontent.com/iorellana21/code-quiz/main/assets/scores.png "code-quiz-score-page")
+
+
+---
+## Credits
+* [Stack Overflow](https://stackoverflow.com/)
+* [W3Schools](https://www.w3schools.com/)
+* [Google](https://www.google.com/)
+
+---
+## Links
+#### Live URL
+* https://iorellana21.github.io/code-quiz/
+#### GitHub URL
+* https://github.com/iorellana21/code-quiz
--- script.js
+++ script.js
@@ -5,11 +5,38 @@ var todayDate = today.toDateString();
 // add text to page to display
 $("#currentDay").text(todayDate);
 
-
+// variable to contain all tags with row class
+var rowColor = $(".row");
 
 // create an array and check if there is data in the 'users' key in localStorage
 // if true, expand the array and add another index; else, create first index
 var userArray = JSON.parse(localStorage.getItem(`${todayDate}`)) || [];
+
+// display current hour
+var currentHour = new Date().getHours();
+console.log("current hour: " + currentHour);
+
+// going through all rows and applying color based on the the rows hour value
+for (var i = 0; i < rowColor.length; i++) {
+    // display value for each row
+    var hourValue = parseInt(rowColor[i].getAttribute("value"));
+    // console.log("row " + rowColor[i].getAttribute("value") + " hour value variable - " + hourValue);
+
+    // compare hour of row and current hour
+    if (currentHour > hourValue) {
+        // apply past class to row if current hour is greater than rows hour value
+        var pastHours = rowColor[i].setAttribute("class", "past");
+
+        // apply future class to row if current hour is less than rows hour value
+    } else if (currentHour < hourValue) {
+        var futureHours = rowColor[i].setAttribute("class", "future");
+
+        // otherwise apply present class
+    } else {
+        var presentHour = rowColor[i].setAttribute("class", "present");
+    }
+
+}
 
 // what happens when Save button is clicked
 $("button").on("click", function (event) {
@@ -44,3 +71,31 @@ $("button").on("click", function (event) {
     localStorage.setItem(`${todayDate}`, JSON.stringify(userArray));
 
 });
+
+
+// display task from local storage to page
+function loadData() {
+    // retrieve data in local storage key and store into an array of objects
+    var getTasks = JSON.parse(localStorage.getItem(`${todayDate}`));
+    console.log(getTasks);
+
+    // check if there was data in local storage
+    if (getTasks !== null) {
+        userArray = getTasks;
+        console.log(userArray);
+    } else {
+        return;
+    }
+
+    // push saved events back onto page by appending if the key is equal to the index hour of the array object
+    var keys = Object.entries(getTasks);
+    console.log(keys);
+    keys.forEach(([key, value]) => {
+        for (var i = key; i < 18; i++)
+            if ($("#input-" + i).attr('data-time') === key) {
+                $('#input-' + i).append(value);
+            }
+    })
+}
+
+loadData();
